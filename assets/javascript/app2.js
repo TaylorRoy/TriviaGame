203239b37ee1b10f360e7e1c8354d6dfe826733d
TaylorRoy <tbroy@hotmail.com>
2018-08-26 11:26:34 -0600

    need to add unanswered and css

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -49,9 +49,12 @@ $(".btn").on("click", run);
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(countdown, 1000);
-
     //populates DOM with questions and answers from questions array above
     addQuestionAnswer(questions);
+    //adds a break between questions and done button
+    $("#question-area").append("<br>"); 
+    //adds done button 
+    addDoneButton();
 };
 
 
@@ -67,7 +70,8 @@ function addQuestionAnswer(questions) {
         $("#question-area").append("<input class='answer' id='" + questions[i].answers[0] + "' type='radio' name='question-"+ i +"' data-questionNumber=" + i + " value='" + questions[i].answers[0] + "'>" + questions[i].answers[0]);
         $("#question-area").append("<input class='answer' id='" + questions[i].answers[1] + "' type='radio' name='question-"+ i +"' data-questionNumber=" + i + " value='" + questions[i].answers[1] + "'>" + questions[i].answers[1]);
         $("#question-area").append("<input class='answer' id='" + questions[i].answers[2] + "' type='radio' name='question-"+ i +"' data-questionNumber=" + i + " value='" + questions[i].answers[2] + "'>" + questions[i].answers[2]);
-    }          
+    } 
+       
 };
 
 //if document is clicked on an area with class= answer, run function to gather value(index in answer array) of radio button(string), set to a number, and compare to correct answer if element is unclicked.  if already clicked, it will return to the program without analyzing additional clicks 
@@ -92,51 +96,53 @@ $(document).on("click", ".answer", function(){
     }
 });
 
-// var radios = document.getElementsByName('question-0');
-// console.log(radios);
-// for (var i = 0, length = radios.length; i < length; i++) {
-//     if (radio[i].checked)
-    
-//  {
-//   // do whatever you want with the checked radio
-// //   alert(NodeList[i].value);
+//listens for clicks on done button.  if clicked, clears html container and adds correct and incorrect values
+$(document).on("click", ".done-button", function(){
+    $(".container").empty();
+    var results = $("<div>");
+    var breakDiv = $("<br>");
+    results.addClass("results");
+    $(".container").append("correct:" + correct);
+    $(".container").append(breakDiv);
+    $(".container").append("incorrect:" + incorrect);
+})
 
-//   // only one radio can be logically checked, don't check the rest
-//   break;
-//  }
-// }
-
-
-//     if ("value" != z[i][3]) {
-// alert('wrong');
-//     }
-
-
-//  The countdown function.
-
+//The countdown function.
 function countdown() {
 
-
-    //  Decrease number by one.
+    //Decrease time by one.
     time--;
 
-    //  Show the number in the #timer-area tag.
+    //Show the number in the #timer-area tag.
     $("#timer-area").html("<h2>" + time + "</h2>");
 
-
-    //  Once number hits zero...
+    //Once time hits zero, empty html container adds a "times up" line, and adds correct and incorrect values
     if (time === 0) {
 
-        //  ...run the stop function.
+        //run the stop function.
         stop();
 
-        //  Alert the user that time is up.
-        alert("correct:" + correct);
-        alert("incorrect:" + incorrect);
-        
+        $(".container").empty();
+        var results = $("<div>");
+        var breakDiv = $("<br>");
+        results.addClass("results");
+        $(".container").html("<h2>Time's Up!</h2>");
+        $(".container").append(breakDiv);
+        $(".container").append("correct:" + correct);
+        $(".container").append(breakDiv);
+        $(".container").append("incorrect:" + incorrect);  
     }
-}
+};
 
+//clear intervalID 
 function stop() {
     clearInterval(intervalId);
+};
+
+//add done button to #question area on html 
+function addDoneButton(){
+    var doneDiv = $("<button>");
+    doneDiv.addClass("done-button");
+    doneDiv.text("DONE!");
+    $("#question-area").append(doneDiv);
 };
